6e0e19764dcfa7476e5232687a4cce72117ee144
Dev <dev@omars-MacBook-Air.local>
2025-02-10 16:23:50 -0500

    Removed Motorcycle class to match class diagram

--- src/TestVehicles.java
+++ src/TestVehicles.java
@@ -3,14 +3,12 @@ public class TestVehicles {
         Car car = new Car("Toyota", "Gasoline", "Red");
         Truck truck = new Truck("Ford", "Diesel", "Blue", 8);
         Bus bus = new Bus("Mercedes", "Electric", "White");
-        Motorcycle bike = new Motorcycle("Yamaha", "Gasoline", "Black", true);
-
+  
         car.displayInfo();
         System.out.println();
         truck.displayInfo();
         System.out.println();
         bus.displayInfo();
         System.out.println();
-        bike.displayInfo();
     }
 }
